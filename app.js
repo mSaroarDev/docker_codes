ade1322562fa94d1ad5ab7e09c44dc10bc02404d
mSaroarDev <msaroar.dev@gmail.com>
2025-04-21 09:26:16 +0600

    docker compose backed added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 app.get("/", (req, res) => {
-    res.status(200).json("Hello Developer! 22");
+    res.status(200).json("Hello Mr. Saroar! 2025");
 });
 
 app.listen(8000, () => {
